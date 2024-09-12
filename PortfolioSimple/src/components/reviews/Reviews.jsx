63b7c37778b9ac5e043175483aa12e5abfc35956
dongdongdongk <dhk9309@gmail.com>
2024-09-12 17:35:33 +0900

    SCSS

--- PortfolioSimple/src/components/reviews/Reviews.jsx
+++ PortfolioSimple/src/components/reviews/Reviews.jsx
@@ -1,13 +1,21 @@
 import "./Reviews.scss"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { AiFillStar } from 'react-icons';
+import { reviewsData } from '../../Data'
 
 
 const Reviews = () => {
   return (
-    <div>
-      
-    </div>
+    <serction className="reviews" id='reviews'>
+        <div className="section__text">
+            <p className="cl-blue">reviews</p>
+            <h2>What My Clients Say</h2>
+        </div>
+        <div className="reviews__container">
+            <Carousel></Carousel>
+        </div>
+    </serction>
   )
 }
 
