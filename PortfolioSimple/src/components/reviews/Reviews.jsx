72eb069d8805289048e95934a0f39e9a31aabece
dongdongdongk <dhk9309@gmail.com>
2024-09-12 17:23:30 +0900

    SCSS

--- PortfolioSimple/src/components/reviews/Reviews.jsx
+++ PortfolioSimple/src/components/reviews/Reviews.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import "./Reviews.scss"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Reviews = () => {
   return (
