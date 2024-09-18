43e1fe528eeb5f14ba9b66386c4d467c825c4da3
dongdongdongk <dhk9309@gmail.com>
2024-09-18 16:59:45 +0900

    pj2

--- TravelWebsite/src/components/Popular/Popular.jsx
+++ TravelWebsite/src/components/Popular/Popular.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
 
 export const Popular = () => {
     return (
-        <div>Popular</div>
+        <section className="popular section container">
+            <div className="secContainer">
+                <div className="secHeader flex">
+                    <div className="textDiv">
+                        <h2 className="secTitle">
+                            Popular Destination
+                        </h2>
+                        <p>
+                            From histroycal citieirs to narltuf
+                            sperckthr coen set the vest of  the worle!
+                        </p>
+                    </div>
+                    <div className="iconsDiv flex">
+                        <BsArrowLeftShort className='icon'/>
+                        <BsArrowRightShort className='icon'/>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
