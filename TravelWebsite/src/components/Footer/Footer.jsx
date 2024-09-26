4e3c1133ac76831cb2a70c00e05f658cdc3b9e9c
dongdongdongk <dhk9309@gmail.com>
2024-09-26 14:22:29 +0900

    Footer

--- TravelWebsite/src/components/Footer/Footer.jsx
+++ TravelWebsite/src/components/Footer/Footer.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import './footer.scss'
+import { MdTravelExplore } from "react-icons/md";
+import { ImFacebook } from "react-icons/im";
+import { BsTwitter } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="secContainer container grid">
+        <div className="logoDiv">
+          <div className="footerLogo">
+            <a href="" className="logo flex">
+              <h1 className='flex'><MdTravelExplore className="icon" />
+                Dot</h1>
+            </a>
+          </div>
+
+          <div className="socials flex">
+            <ImFacebook className='icon' />
+            <BsTwitter className='icon' />
+            <AiFillInstagram className='icon' />
+          </div>
+        </div>
+        <div className="footerLinks">
+          <span className="linkTitle">
+            Information
+          </span>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <a href="#">Travel</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </div>
+
+
+        <div className="footerLinks">
+          <span className="linkTitle">
+            Contact Us
+          </span>
+          <span className="phone">+444 555 4444</span>
+          <span className="email">fjg9403@fnag.com</span>
+        </div>
+
+
+
+      </div>
+    </div>
   )
 }
